22b6faa8de36b446134d05c0bfcdea9996cc1dc8
Serhat can Bakır <127730682+SerhatCanBakir@users.noreply.github.com>
2024-05-06 17:51:03 +0300

    js codes
    add remove

---
+++ javakods/index.js
@@ -0,0 +1,31 @@
+var idmaker = 1;
+console.log('deneme');
+function ekle() {
+    var yazi = document.getElementById("taskInput")
+    var ekleme = document.createElement('li');
+    var yazisi = document.createTextNode(yazi.value);
+    var label = document.createElement("label");
+    label.setAttribute("class", "lbl");
+    label.setAttribute("id", idmaker.toString);
+    ekleme.appendChild(yazisi);
+    label.appendChild(ekleme);
+    var list = document.getElementById('taskList');
+    list.appendChild(label);
+    idmaker++;
+}
+
+var addbutton = document.getElementById("addbutton");
+addbutton.addEventListener("click", () => ekle());
+var deletebutton = document.getElementById("deletebutton");
+deletebutton.addEventListener("click", () => {
+    var idcol = document.getElementById("taskInput");
+    sil(idcol.value);
+})
+function alarm() { alert('deneme') }
+
+function sil(id) {
+    var parents = document.getElementById('taskList');
+    var ch = document.getElementById(id.toString);
+    var bisi = parents.removeChild(ch);
+
+}
