10fafefd3c6220d4ff24ba639268db503d4d3b8f
SerhatCanBakir <serhat2534serhat@outlook.com>
2024-05-06 19:44:41 +0300

    her sefeinde id düzenlencek sekilde kod düzeltildi

--- javakods/index.js
+++ javakods/index.js
@@ -1,16 +1,21 @@
 var idmaker = 1;
 console.log('deneme');
 function ekle() {
+    var list = document.getElementById('taskList');
     var yazi = document.getElementById("taskInput")
     var ekleme = document.createElement('li');
     var yazisi = document.createTextNode(yazi.value);
     var label = document.createElement("label");
     label.setAttribute("class", "lbl");
-    label.setAttribute("id", idmaker.toString);
+    label.setAttribute("id", (list.childElementCount));
+    label.addEventListener("click",()=>{
+        yazi.value=label.getAttribute('id');
+});
     ekleme.appendChild(yazisi);
     label.appendChild(ekleme);
-    var list = document.getElementById('taskList');
+    
     list.appendChild(label);
+    
     idmaker++;
 }
 var addbutton = document.getElementById("addbutton");
@@ -25,5 +30,9 @@ function alarm() { alert('deneme') }
 function sil(element) {
     var parents = document.getElementById('taskList');
     var bisi = parents.removeChild(parents.children[element]);
-
+    for(let i=0;i<parents.childElementCount;i++)
+        {
+           var deg = parents.children[i];
+           deg.setAttribute('id',i);
+        }
 }
