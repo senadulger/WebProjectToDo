5320b67ae99badac4de7d9839c32320adb3b9dad
SerhatCanBakir <serhat2534serhat@outlook.com>
2024-05-12 13:37:55 +0300

    birden fazla secme islemi gitti

--- javakods/index.js
+++ javakods/index.js
@@ -1,101 +1,114 @@
-var selectedOne;
-if(localStorage.getItem('list')==null){
-localStorage.setItem('list',"[]");}
-function GetToLocal(){
-var Temp = JSON.parse(localStorage.getItem('list'));
- return Temp;
+var selectedOne = NaN;
+if (localStorage.getItem('list') == null) {
+    localStorage.setItem('list', "[]");
+}
+function GetToLocal() {
+    var Temp = JSON.parse(localStorage.getItem('list'));
+    return Temp;
 }
 
-function AddToLocal(Task){
-  var Temp = [];
-  
-  Temp=GetToLocal()
-  
-  var element ={'todo':Task,'done':false};
-  Temp.push(element);
-  localStorage.setItem('list',JSON.stringify(Temp));
+function AddToLocal(Task) {
+    var Temp = [];
+
+    Temp = GetToLocal()
+
+    var element = { 'todo': Task, 'done': false };
+    Temp.push(element);
+    localStorage.setItem('list', JSON.stringify(Temp));
 
 }
-function CreateToList(){
-    
-    var temp = GetToLocal()||[];
-    if(Array.isArray(temp)){
-    for(let index = 0; index < temp.length; index++){
-        const element = temp[index];
-        const lbl = document.createElement('label');
-    const listeleman = document.createElement('li');
-    const node = document.createTextNode(element.todo);
-    var list = document.getElementById('taskList');
-    if(element.done == true){
-        lbl.setAttribute('class','taskcompleted');
-    }else{lbl.setAttribute('class','tasknotcompleted')}
-    lbl.setAttribute('id',index);
-    listeleman.appendChild(node);
-    lbl.appendChild(listeleman);
-    lbl.addEventListener('click',()=>{
-       selectedOne=lbl.getAttribute('id');
-        })
-        
-    lbl.addEventListener('contextmenu',()=>{
-        EditTheList(lbl.getAttribute('id'));
-        console.log('sag tiklandi');        
-    });
+function CreateToList() {
 
-    
-    list.appendChild(lbl);
-    
-        
-    }}
+    var temp = GetToLocal() || [];
+    if (Array.isArray(temp)) {
+        for (let index = 0; index < temp.length; index++) {
+            const element = temp[index];
+            const lbl = document.createElement('label');
+            const listeleman = document.createElement('li');
+            const node = document.createTextNode(element.todo);
+            var list = document.getElementById('taskList');
+            if (element.done == true) {
+                lbl.setAttribute('class', 'taskcompleted');
+            } else { lbl.setAttribute('class', 'tasknotcompleted') }
+            lbl.setAttribute('id', index);
+            listeleman.appendChild(node);
+            lbl.appendChild(listeleman);
+            lbl.addEventListener('click', () => {
+                selectedOne = lbl.getAttribute('id');
+                var rick = document.getElementsByClassName('task').length;
+                if(lbl.getAttribute('class')=='task'||rick>=1){
+                    RemoveTheList()
+                    CreateToList();
+                }else{lbl.setAttribute('class','task');}
+            
+            })
+
+            lbl.addEventListener('contextmenu', () => {
+                EditTheList(lbl.getAttribute('id'));
+                console.log('sag tiklandi');
+            });
+
+
+            list.appendChild(lbl);
+
+
+        }
+    }
 }
-function RemoveTheList(){
+function RemoveTheList() {
     const list = document.getElementById('taskList');
     var len = list.childElementCount;
-   while(list.firstChild){
-    list.firstChild.remove();
-   }}
+    while (list.firstChild) {
+        list.firstChild.remove();
+    }
+}
 CreateToList();
-document.getElementById('bttn').addEventListener('click',()=>{
+document.getElementById('bttn').addEventListener('click', () => {
     var textfield = document.getElementById('taskInput');
     AddToLocal(textfield.value);
     RemoveTheList();
     CreateToList();
 })
-function RemoveTheLocal(id){
-   var loc = GetToLocal();
-   if(id!=NaN){
-   loc.splice(id,1);}else{loc = [];}
-   localStorage.setItem('list',JSON.stringify(loc));
-   RemoveTheList();
-   CreateToList();
-}
+function RemoveTheLocal(id) {
 
-function EditTheList(id){
-  var loc = GetToLocal();
-  var a =prompt('Yeni Taski giriniz');
-  
-  loc[id].todo=a;
-  localStorage.setItem('list',JSON.stringify(loc));
-  RemoveTheList();
-  CreateToList();
-}
-
-function ChangeToComp(id){
-    var loc = GetToLocal();
-    if(loc[id].done==true){loc[id].done=false}else{loc[id].done=true}
-    localStorage.setItem('list',JSON.stringify(loc));
+    if (id == NaN) {
+        var loc = [];
+    } else {
+        var loc = GetToLocal();
+        loc.splice(id, 1);
+    }
+    localStorage.setItem('list', JSON.stringify(loc));
     RemoveTheList();
     CreateToList();
 }
 
-document.addEventListener("contextmenu",(e)=>{
+function EditTheList(id) {
+    var loc = GetToLocal();
+    var a = prompt('Yeni Taski giriniz');
+
+    loc[id].todo = a;
+    localStorage.setItem('list', JSON.stringify(loc));
+    RemoveTheList();
+    CreateToList();
+}
+
+function ChangeToComp(id) {
+    var loc = GetToLocal();
+    if (loc[id].done == true) { loc[id].done = false } else { loc[id].done = true }
+    localStorage.setItem('list', JSON.stringify(loc));
+    RemoveTheList();
+    CreateToList();
+}
+
+document.addEventListener("contextmenu", (e) => {
 
     e.preventDefault();
 })
-document.getElementById('deletebttn').addEventListener('click',()=>{
-RemoveTheLocal(selectedOne);
-selectedOne = NaN;
+document.getElementById('deletebttn').addEventListener('click', () => {
+    RemoveTheLocal(selectedOne);
+    selectedOne = NaN;
 })
-document.getElementById('donebttn').addEventListener('click',()=>{
+document.getElementById('donebttn').addEventListener('click', () => {
     ChangeToComp(selectedOne);
-    selectedOne=NaN;
+    selectedOne = NaN;
 })
